2e7a810aa541150156f3f7a0a16ad0c6a6185b05
marie28 <mcalvo@pernixlabs.com>
2021-09-22 11:30:24 -0600

    Added portafolio itens via Resource generator

---
+++ db/migrate/20210922172230_create_portafolios.rb
@@ -0,0 +1,14 @@
+class CreatePortafolios < ActiveRecord::Migration[6.1]
+  def change
+    create_table :portafolios do |t|
+      t.string :title
+      t.string :subtitle
+      t.string :string
+      t.text :body
+      t.text :main_image
+      t.text :thumb_image
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_22_155429) do
+ActiveRecord::Schema.define(version: 2021_09_22_172230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,17 @@ ActiveRecord::Schema.define(version: 2021_09_22_155429) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "portafolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.string "string"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
