77beaf7b7dde2c686498e79f9f9be884ebef9083
marie28 <mcalvo@pernixlabs.com>
2021-09-22 14:43:33 -0600

    Integrated new and create actions for portafilios

--- app/controllers/portafolios_controller.rb
+++ app/controllers/portafolios_controller.rb
@@ -3,5 +3,24 @@ class PortafoliosController < ApplicationController
   def index
     @portfolio_items = Portafolio.all
   end
+
+  def new
+    @portafolio = Portafolio.new
+  end
+  
+  # POST /blogs or /blogs.json
+  def create
+    @portafolio = Portafolio.new(params.require(:portafolio).permit(:title,:subtitle ,:body))
+
+    respond_to do |format|
+      if @portafolio.save
+        format.html { redirect_to portafolios_path, notice: "Blog was successfully created." }
+        format.json { render :show, status: :created, location: @portafolio }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @portafolio.errors, status: :unprocessable_entity }
+      end
+    end
+  end
   
 end
